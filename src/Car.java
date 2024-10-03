6610353f4c7dc8689884a2e01f473a4b2af98f41
telfort92 <telfortps@outlook.com>
2024-10-02 20:23:48 -0500

    Added 2 classes as draft for project

---
+++ src/Car.java
@@ -0,0 +1,13 @@
+/**
+ * Project 1
+ *
+ * @author Telfort
+ *
+ * @created 25 Aug 2024
+ *
+ * @summary This class will be used to create a car object.
+ */
+
+public class Car {
+
+} // End of Car class
---
+++ src/TrafficSimulator.java
@@ -0,0 +1,16 @@
+/**
+ * Project 1
+ *
+ * @author Telfort
+ *
+ * @created 25 Aug 2024
+ *
+ * @summary This class will be used to simulate traffic
+ */
+public class TrafficSimulator {
+
+    public static void main(String[] args) {
+
+    } // End on main
+
+} // End of traffic simulator class
